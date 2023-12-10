f3df4008f46b26904d9fdd9e696e0d1847c656fa
simon <simon.vrouwenvelder@tno.nl>
2023-12-10 22:06:45 +0100

    part 2 of day 9

--- Day9/day9.py
+++ Day9/day9.py
@@ -1,29 +1,45 @@
 import re
 from typing import Final
+
 import numpy as np
 from numpy.typing import NDArray
 
 PATH_TO_FILE: Final[str] = "./Day9/input_file.txt"
-PATH_TO_TEST_FILE: Final[str] = "./Day9/test_file.txt"
+
 
 def extract_numbers(record: str) -> list[int]:
-    return [int(i) for i in re.findall(r'-\d+|\d+', record)]
+    return [int(i) for i in re.findall(r"-\d+|\d+", record)]
 
-def predict(numbers: list[int]) -> int:
-    deltas: list[NDArray[np.int32]] = [np.diff(numbers)]
-    while deltas[-1].size != 1 and not np.allclose(deltas[-1], 0):
-        deltas.append(np.diff(deltas[-1]))
 
+def predict_forwards(numbers: list[int]) -> int:
     delta = 0
-    for diff in reversed(deltas):
+    for diff in reversed(_get_differences(numbers)):
         delta = diff[-1] + delta
     return numbers[-1] + delta
 
 
-def get_summed_prediction(path_to_file: str) -> int:
-    data = open(path_to_file, 'r').read().rstrip().splitlines()
-    return sum(predict(extract_numbers(numbers)) for numbers in data)
+def predict_backwards(numbers: list[int]) -> int:
+    delta = 0
+    for diff in reversed(_get_differences(numbers)):
+        delta = diff[0] - delta
+    return numbers[0] - delta
+
+
+def _get_differences(numbers: list[int]) -> list[NDArray[np.int32]]:
+    deltas: list[NDArray[np.int32]] = [np.diff(numbers)]
+    while deltas[-1].size != 1 and not np.allclose(deltas[-1], 0):
+        deltas.append(np.diff(deltas[-1]))
+    return deltas
+
+
+def get_summed_prediction(path_to_file: str, prediction) -> int:
+    data = open(path_to_file, "r").read().rstrip().splitlines()
+    return sum(prediction(extract_numbers(numbers)) for numbers in data)
+
 
 if __name__ == "__main__":
     print("PART 1")
-    print(get_summed_prediction(PATH_TO_FILE))
+    print(get_summed_prediction(PATH_TO_FILE, predict_forwards))
+
+    print("PART 2")
+    print(get_summed_prediction(PATH_TO_FILE, predict_backwards))
