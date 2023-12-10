3930b61891c703f1157c116319d5459d6efcb7ab
simon <simon.vrouwenvelder@tno.nl>
2023-12-10 21:14:10 +0100

    day 8, part 2

--- Day8/day8.py
+++ Day8/day8.py
@@ -1,9 +1,10 @@
+import math
 import re
+from collections.abc import Callable
 from dataclasses import dataclass
 from typing import Final
 
 PATH_TO_FILE: Final[str] = "./Day8/input_file.txt"
-PATH_TO_TEST_FILE: Final[str] = "./Day8/test_file.txt"
 
 
 @dataclass
@@ -14,7 +15,7 @@ class Node:
 
 
 def create_node(record: str) -> Node:
-    node_info = re.findall(r"[A-Z]+", record)
+    node_info = re.findall(r"[\w\d]+", record)
     return Node(node_info[0], node_info[1], node_info[2])
 
 
@@ -29,11 +30,16 @@ def load_data(path_to_file: str) -> tuple[str, dict[str, Node]]:
     return command, nodes
 
 
-def traverse_to_ZZZ(command: str, nodes: dict[str, Node], start: str) -> int:
+def traverse(
+    command: str,
+    nodes: dict[str, Node],
+    start: str,
+    terminal_condition: Callable[[str], bool],
+) -> int:
     step = 0
     node = nodes[start]
     count = 0
-    while node.name != "ZZZ":
+    while not terminal_condition(node.name):
         if step == len(command):
             step = 0
 
@@ -47,7 +53,20 @@ def traverse_to_ZZZ(command: str, nodes: dict[str, Node], start: str) -> int:
     return count
 
 
+def get_starting_nodes(nodes: list[str]) -> list[str]:
+    return [node for node in nodes if node[-1] == "A"]
+
+
 if __name__ == "__main__":
     print("PART 1")
+    terminal_condition = lambda x: x == "ZZZ"
     command, nodes = load_data(PATH_TO_FILE)
-    print(traverse_to_ZZZ(command, nodes, "AAA"))
+    print(traverse(command, nodes, "AAA", terminal_condition))
+
+    print("PART 2")
+    terminal_condition = lambda x: x[-1] == "Z"
+    loops = [
+        traverse(command, nodes, start, terminal_condition)
+        for start in get_starting_nodes(list(nodes.keys()))
+    ]
+    print(math.lcm(*loops))
